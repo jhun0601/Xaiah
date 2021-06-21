2eb67e66bcb1017da4e65654121716452c7cfe31
jhunmark <jhunmark@jobs180.com>
2021-06-21 17:40:49 +0800

    install cors morgan and express json

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+
+const morgan = require('morgan');
+
 const connectDb = require('./database/db');
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+
 
 connectDb();
 
