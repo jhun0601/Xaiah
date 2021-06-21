1c157bbcace3749fed57a9868cad22abe38fa3cc
jhunmark <jhunmark@jobs180.com>
2021-06-21 10:54:59 +0800

    update signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/Signup.css';
-// import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const[formData, setFormData] = useState({
+        username:'',
+        email:'',
+        password:'',
+        password2:'',
+        successMsg:false,
+        errorMsg:false,
+        loading:false
+    })
+    const {username, email, password, password2, successMsg, errorMsg, loading} = formData;
+
+    /******  
+        event handlers
+     ******/
+    const handleChange = (e) => {
+        // console.log(e);
+        setFormData({
+            ...formData, 
+            [e.target.name] : e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData)
+
+    }
     const showSignUpForm = () => (
-        <form className="signup-form">
+        <form className="signup-form" onSubmit={handleSubmit}>
             <div className="form-group mb-2" >
                 <div className="input-group flex-nowrap">
                     <span className="input-group-text" id="addon-wrapping"><i className="fa fa-user"></i></span>
-                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" />
+                    <input name="username" value={username} type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" onChange={handleChange}/>
                 </div>
             </div>
             <div className="form-group mb-2" >
                 <div className="input-group flex-nowrap">
                     <span className="input-group-text" id="addon-wrapping"><i className="fa fa-envelope"></i></span>
-                    <input type="email" className="form-control" placeholder="Email address" aria-label="Email address" aria-describedby="addon-wrapping" />
+                    <input name="email" value={email} type="email" className="form-control" placeholder="Email address" aria-label="Email address" aria-describedby="addon-wrapping" onChange={handleChange}/>
                 </div>
             </div>
             <div className="form-group mb-2" >
                 <div className="input-group flex-nowrap">
                     <span className="input-group-text" id="addon-wrapping"><i className="fa fa-lock"></i></span>
-                    <input type="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping" />
+                    <input name="password" value={password} type="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping" onChange={handleChange}/>
                 </div>
             </div>
             <div className="form-group mb-2" >
                 <div className="input-group flex-nowrap">
                     <span className="input-group-text" id="addon-wrapping"><i className="fa fa-lock"></i></span>
-                    <input type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="addon-wrapping" />
+                    <input name="password2" value={password2} type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="addon-wrapping" onChange={handleChange}/>
                 </div>
             </div>
             
@@ -34,7 +60,7 @@ const Signup = () => {
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
             </div>
             <p className="text-center text-white mt-2">
-                Have an account? <a href="">Log In</a>
+                Have an account? <Link to="/signin">Log In</Link>
             </p>
 
         </form>
@@ -45,6 +71,7 @@ const Signup = () => {
             <div className="row px-3 vh-100">
                 <div className="col-md-5 mx-auto align-self-center">
                     {showSignUpForm()}
+                    <p style={{color:'white'}}>{JSON.stringify(formData)}</p>
                 </div>
             </div>
         </div>
