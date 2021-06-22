f252e441775bbcc9ef5cef3f25aecc5ec4bec1c6
jhunmark <jhunmark@jobs180.com>
2021-06-22 10:55:18 +0800

    add routes

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
-
 const app = express();
-
 const cors = require('cors');
-
 const morgan = require('morgan');
+const authRoutes =require('./routes/auth');
 
 const connectDb = require('./database/db');
 
 //middleware
 app.use(cors());
 app.use(morgan('dev'));
-
 app.use(express.json());
-
+//
+app.use('/api/auth', authRoutes);
 
 connectDb();
+//api
+app.get('/', (req, res) => {
+    res.send('Inside Server')
+})
+
 
 const port = process.env.PORT || 5000;
 
